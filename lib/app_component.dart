46147213e5c136467ff27a6a41073c1b51a67030
RubbaBoy <adamyarris@yahoo.com>
2018-07-29 13:58:10 -0400

    Initial commit

---
+++ lib/app_component.dart
@@ -0,0 +1,16 @@
+import 'package:angular/angular.dart';
+//import 'package:angular_components/angular_components.dart';
+
+// AngularDart info: https://webdev.dartlang.org/angular
+// Components info: https://webdev.dartlang.org/components
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [],
+  providers: const [],
+)
+class AppComponent {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
---
+++ web/main.dart
@@ -0,0 +1,8 @@
+import 'package:sass_builder/sass_builder.dart';
+import 'package:angular/angular.dart';
+
+import 'package:RubbaBoyWebsiteNew/app_component.template.dart' as ng;
+
+void main() {
+  runApp(ng.AppComponentNgFactory);
+}
